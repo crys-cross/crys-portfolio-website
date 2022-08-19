4ca682de606f59cc4700a1b588159ba282749af4
CRYS <mariano.christopher123@gmail.com>
2022-08-19 22:07:54 +0800

    added template

---
+++ components/Header.jsx
@@ -0,0 +1,16 @@
+// Import Assets
+// import profile from "../assets/profile.png"
+
+const Header = () => (
+    <section className="header">
+        {/* <img src={profile} alt="Christopher Mariano" /> */}
+
+        <div className="header__content">
+            <h1>Hi, I'm Christopher Mariano</h1>
+            <p>Dapp Developer/ Fullstack Web3 Developer</p>
+            <button className="button">Get In Touch</button>
+        </div>
+    </section>
+)
+
+export default Header
---
+++ constants/index.js
@@ -0,0 +1,23 @@
+// import here
+
+export const projectInfo = [
+    {
+        id: info,
+        projectName: "Project Name:",
+        about: "About:",
+        appLink: "App Link:",
+        frontendCode: "FrontendCode",
+        backendCode: "Backend Code",
+    },
+]
+
+export const project1 = [
+    {
+        id: projectFill1,
+        projectName: "Project Name:",
+        about: "About:",
+        appLink: "App Link:",
+        frontendCode: "FrontendCode",
+        backendCode: "Backend Code",
+    },
+]
